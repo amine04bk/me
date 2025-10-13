ac9720749c6e2bc0d6a500b11771f9c7b89331f6
amine04bk <aminebenkhoud@gmail.com>
2025-10-13 23:59:44 +0100

    portfolio

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true }, 
-  basePath: '/portfolio', 
-  assetPrefix: '/portfolio/',
+
  };
 
 export default nextConfig;
